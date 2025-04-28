480855dfd1db83368a98463892aab24ce548bba3
arifulhaque313 <asajib7654@gmail.com>
2025-04-29 00:02:32 +0600

    :up: Update --> Complain Controller and service is completed

--- src/main/java/com/practice/speakup/services/ComplainService.java
+++ src/main/java/com/practice/speakup/services/ComplainService.java
@@ -1,4 +1,81 @@
 package com.practice.speakup.services;
 
+import com.practice.speakup.dtos.ComplainRequest;
+import com.practice.speakup.dtos.ComplainResponse;
+import com.practice.speakup.models.Complain;
+import com.practice.speakup.models.User;
+import com.practice.speakup.repositories.ComplainRepository;
+import com.practice.speakup.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class ComplainService {
+
+    private final ComplainRepository complainRepository;
+    private final UserRepository userRepository;
+
+    public User getCurrentUser(){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    public ComplainResponse createComplain(ComplainRequest request){
+        User user = getCurrentUser();
+        Complain complain = Complain.builder()
+                .title(request.getTitle())
+                .description(request.getDescription())
+                .user(user)
+                .build();
+
+        Complain saved = complainRepository.save(complain);
+        return  mapToResponse(saved);
+    }
+
+    public List<ComplainResponse> getUserComplains(){
+        User user = getCurrentUser();
+        return complainRepository.findByUser()
+                .stream()
+                .map(this::mapTOResponse)
+                .collect(Collectors.toList());
+    }
+
+    public ComplainResponse getUserComplainById(Long id){
+        User user = getCurrentUser();
+        Complain complain = complainRepository.findByIdAndUser(id, user)
+                .orElseThrow(() -> new RuntimeException("Complain no found or does not belongs to the user"));
+                return mapToResponse(complain);
+    }
+
+    public  ComplainResponse updateComplain(Long id, ComplainRequest request){
+        User currentUser = getCurrentUser();
+        Complain complain = complainRepository.findByIdAndUser(id, currentUser)
+                .orElseThrow(() -> new RuntimeException("Complain not found or dose not belongs to the user"));
+        return mapToResponse(complainRepository.save(complain));
+    }
+
+    public  void deleteComplain(Long id){
+        User currentUser = getCurrentUser();
+        Complain complain = complainRepository.findByIdAndUser(id, currentUser)
+                .orElseThrow(() -> new RuntimeException("Complain not found or dose not belongs to the user"));
+
+        complainRepository.delete(complain);
+    }
+
+    private ComplainResponse mapToResponse(Complain complain){
+        return new ComplainResponse(
+                complain.getId(),
+                complain.getTitle(),
+                complain.getDescription(),
+                complain.getUser().getName(),
+                complain.getUser().getUsername()
+        );
+    }
 }
--- src/main/java/com/practice/speakup/services/PDFGenerateService.java
+++ src/main/java/com/practice/speakup/services/PDFGenerateService.java
@@ -1,4 +1,34 @@
 package com.practice.speakup.services;
 
+import com.practice.speakup.dtos.ComplainResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.naming.Context;
+import java.io.ByteArrayOutputStream;
+
+@Service
 public class PDFGenerateService {
-}
+    private  final TemplateEngine templateEngine;
+
+    @Autowired
+    public PDFGenerateService(TemplateEngine templateEngine){
+        this.templateEngine = templateEngine;
+    }
+
+    public  byte[] generatePdf(ComplainResponse complainResponse) throws Exception{
+        Context context = new Context();
+        context.setVariable("complain", complainResponse);
+
+        String htmlContent = templateEngine.process("complain_template", context);
+
+        ITextRendered rendered = new ITextRenderer();
+        renderer.setDocumentFromString(htmlContent);
+        renderer.layout();
+
+        try(ByteArrayOutputStream outputStream = new ByteArrayOutputStream()){
+            rendered.createPDF(outputStream);
+            return outputStream.toByteArray();
+        }
+    }
+ }
